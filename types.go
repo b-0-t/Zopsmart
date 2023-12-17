d6fc8dc3a84488416a67f7762ea27035de70b1ea
Dhruv Jain <dhruvjn110327@gmail.com>
2023-12-18 05:18:48 +0530

    all set

--- types.go
+++ types.go
@@ -7,6 +7,27 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+type LoginResponse struct {
+	Number int64  `json:"number"`
+	Token  string `json:"token"`
+}
+
+type LoginRequest struct {
+	Number   int64  `json:"number"`
+	Password string `json:"password"`
+}
+
+type TransferRequest struct {
+	ToAccount int `json:"toAccount"`
+	Amount    int `json:"amount"`
+}
+
+type CreateAccountRequest struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Password  string `json:"password"`
+}
+
 type Account struct {
 	ID                int       `json:"id"`
 	FirstName         string    `json:"firstName"`
@@ -15,6 +36,10 @@ type Account struct {
 	EncryptedPassword string    `json:"-"`
 	Balance           int64     `json:"balance"`
 	CreatedAt         time.Time `json:"createdAt"`
+}
+
+func (a *Account) ValidPassword(pw string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(a.EncryptedPassword), []byte(pw)) == nil
 }
 
 func NewAccount(firstName, lastName, password string) (*Account, error) {
