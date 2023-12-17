7162aad86604a971c841cd20299ab0ffbc40b5ad
Dhruv Jain <dhruvjn110327@gmail.com>
2023-12-18 01:58:36 +0530

    writejson fnc

--- api.go
+++ api.go
@@ -5,6 +5,14 @@ import {
 	"github.com/gorilla/mux"
 }
 
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	return json.NewEncoder(w).Encode(v)
+}
+
+
 type apiFunc func(http.ResponseWriter, *http.Request) error
 
 type ApiError struct {
